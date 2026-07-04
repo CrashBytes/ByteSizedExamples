cc555d3a2fadeaf020dc8e1e8b46a393926297ca
CrashBytes-Personal <crashbytes@blackholesoftware.com>
2026-07-04 00:49:47 -0400

    fix(security): remove unused chromadb dep (CVE-2026-45829, no patch); migrate localization tutorial to Storybook 10 for vite 8 compat

--- localization-tutorial/react-i18next-example/.storybook/preview.tsx
+++ localization-tutorial/react-i18next-example/.storybook/preview.tsx
@@ -1,4 +1,4 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react-vite';
 import { I18nextProvider } from "react-i18next";
 import i18n from "../src/i18n/config";
 import React from "react";
